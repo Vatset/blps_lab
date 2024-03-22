1679cd3698430e0c14d0fa049a1fa3ca69aee80b
Maria Vatset <333885@niuitmo.ru>
2024-03-23 01:16:16 +0300

    init

---
+++ src/main/java/com/lab/blps_lab/BlpsLabApplication.java
@@ -0,0 +1,13 @@
+package com.lab.blps_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlpsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlpsLabApplication.class, args);
+	}
+
+}
